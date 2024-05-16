dbe94e781f1d48c134623c64dce2eb3a1d33ccbb
quantankevin <kevin980034@gmail.com>
2024-05-16 23:04:44 +0800

    Define pos_distrib,vel_distrib,set_particles in Particle.py and  plot_velocity_distribution,plot_position_distribution in DataProcesser.py. Meanwhile, I wrote some codes to test previous functions. It shows how to operate the function I made.

--- functions/DataProcesser.py
+++ functions/DataProcesser.py
@@ -1,3 +1,7 @@
+import numpy as np
+import scipy.constants as const
+import scipy.stats as stats
+import matplotlib.pyplot as plt
 class DataProcesser:
     """
     This class will analyse the data from the simulation.
@@ -6,6 +10,71 @@ class DataProcesser:
         #TODO
         pass
     
+    @staticmethod
+    def plot_velocity_distribution(temperature:float, mass:float, vel:list):
+        """
+        This function will plot the velocity distribution of the particles.
+        Temperature: The temperature of the room.
+        Mass: The mass of the particles.
+        vel: The velocity vector of the particles.
+        """
+        #TODO
+        #plot the histogram of the data
+        speeds = np.linalg.norm(vel, axis=1) #calculate the magnitude of the speeds
+        plt.hist(speeds, bins=50, alpha=0.6, color='g', density=True, label='Simulation') #plot the histogram of the data
+        
+        #create the theoretical distribution
+        speeds_range = np.linspace(0, np.max(speeds), 100) #create 100 points from 0 to max speed
+        theory_distribution = stats.maxwell.pdf(speeds_range, scale=np.sqrt(const.k*temperature / mass)) #apply the maxwell distribution
+        plt.plot(speeds_range, theory_distribution, 'r--', label='Theory')
+        plt.title('Speed Distribution')
+        plt.xlabel('Speed (m/s)')
+        plt.ylabel('Probability Density')
+        plt.legend()
+        plt.show()
+
+    @staticmethod
+    def plot_position_distribution(pos:list,room_size:list, Nsection=5):
+        """
+        This function will plot the position distribution of the particles.
+        Position: The position of the particles.
+        room_size: The size of the room [xmin,xmax,ymin,ymax].
+        Nsection: The number of sections to divide the room.
+        """
+        xmin=room_size[0]
+        xmax=room_size[1]
+        ymin=room_size[2]
+        ymax=room_size[3]
+        bins_x=int((xmax-xmin)/Nsection)
+        bins_y=int((ymax-ymin)/Nsection)
+        
+        
+
+        x_average=np.ones(bins_x)*len(pos[:,0])/bins_x
+        y_average=np.ones(bins_x)*len(pos[:,1])/bins_y
+        x=np.linspace(xmin,xmax,bins_x)
+        y=np.linspace(ymin,ymax,bins_x)
+        plt.figure(figsize=(8,12), dpi=80)
+        ax1 = plt.subplot(211)
+        ax1.plot(x, x_average, 'r--', label='Average x')
+        ax1.hist(pos[:,0], bins=bins_x, alpha=0.6, color='g', label='Simulation') #plot the histogram of the x position
+        ax1.set_title('x position Distribution')
+        ax1.set_xlabel('x (m)')
+        ax1.set_ylabel('Number of Particles')
+        ax1.legend()
+        ax2 = plt.subplot(212)
+        ax2.plot(y, y_average, 'r--', label='Average y')  
+        ax2.hist(pos[:,1], bins=bins_y, alpha=0.6, color='b', label='Simulation') #plot the histogram of the y position
+        ax2.set_title('y position Distribution')
+        ax2.set_xlabel('y (m)')
+        ax2.set_ylabel('Number of Particles')
+        ax2.legend()
+
+        plt.show()
+        
+        
+
+ 
     @staticmethod
     def data_output():
         """
--- functions/Particles.py
+++ functions/Particles.py
@@ -1,31 +1,84 @@
+import numpy as np
+import scipy.constants as const
+import scipy.stats as stats
 class Particles:
     """
     Class particles is the base on the particles we have on class. It contains the basic properties of particles.
     """
-    def __init__(self, n):
+    def __init__(self,n):
         """
         Create empty lists for every property of the particles.
         """
-        #TODO
-        pass
+        self.nparticles = n
+        self.pos=np.zeros((n,2))
+        self.vel=np.zeros((n,2))
+        return
 
-    def pos_distrib(self,n,type='normal'):
+    
+    def pos_distrib(self):
         """
-        This function will distribute the distribution particles in the room.
+        This function will distribute the particles randomly in certain distribution in the room.
+        Some parameters are needed to calculate the distribution:
+        n: number of particles
+        room_size: size of the room [xmin,xmax,ymin,ymax]
+        type: type of distribution (uniform, normal)
         """
-        #TODO
-        pass
+        room_size=self.room_size
+        n=self.nparticles
+        type=self.pos_type
+        xmin,xmax,ymin,ymax=room_size
+        if type=='uniform':
+            self.pos[:,0]= np.random.uniform(xmin,xmax,(n)) 
+            self.pos[:,1]= np.random.uniform(ymin,ymax,(n))
+        elif type=='normal':
+            self.pos[:,0]= np.random.normal((xmin+xmax)/2,(xmax-xmin)/6,(n))
+            self.pos[:,1]= np.random.normal((ymin+ymax)/2,(ymax-ymin)/6,(n))
+        return
 
-    def vel_distrib(self,T,n,type='Boltzmann'):
+
+
+    def vel_distrib(self):
         """
         This function will calculate the distribution velocity (temperature) of the room.
+        Some parameters are needed to calculate the distribution:
+        T: temperature of the room
+        m: mass of the particles
+        N: number of particles
+        vel_type: type of distribution (Boltzmann)
         """
-        #TODO
-        pass
-
-    def set_particles(self, pos, vel, acc):
+        T=self.T
+        m=self.mass
+        N=self.nparticles
+        v_type=self.vel_type
+        k=const.Boltzmann
+        if v_type=='Boltzmann':
+            speed = stats.maxwell.rvs(scale=np.sqrt(k*T/ m), size=N) #scale=a in https://mathworld.wolfram.com/MaxwellDistribution.html
+            #detailed from https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.maxwell.html#ra6d46ce10274-1
+            theta = np.random.uniform(0, 2*np.pi, N) #random angle
+            self.vel[:,0] = speed*np.cos(theta)
+            self.vel[:,1] = speed*np.sin(theta)
+        else:
+            return ValueError('Distribution type not supported')
+        return
+    def set_particles(self,pos_type='uniform',vel_type='Boltzmann',room_size=[0,50,0,50],T=300,particle_type='air'):
         """
         This function will set the properties of particles.
+        pos_type: type of position distribution (uniform, normal)
+        vel_type: type of velocity distribution (Boltzmann)
+        room_size: size of the room [xmin,xmax,ymin,ymax]
+        T: temperature of the particles
+        particle_type: type of particle (air)
         """
-        #TODO
-        pass
+        self.particle_type=particle_type
+        if particle_type=='air':
+            #average mass of a particle is about 7 times the mass of an alpha particle(He+2)
+            self.mass=const.physical_constants['alpha particle mass'][0]*7 
+        else:   
+            return ValueError('Particle type not supported')
+        self.T=T
+        self.room_size=room_size
+        self.pos_type=pos_type
+        self.vel_type=vel_type
+        self.vel_distrib(self.nparticles,self.T,self.vel_type)
+        self.pos_distrib(self.nparticles,self.room_size,self.pos_type)
+        return

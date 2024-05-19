3907562a76c1df8ca450ede22ecf0a9c4389b813
Gillwu <gillwu.tpe@gmail.com>
2024-05-20 00:26:19 +0800

    add first edition of environment. finished setting init and wall bounce; air_con_suck is still handling.

---
+++ functions/Environment.py
@@ -0,0 +1,85 @@
+import numpy as np
+import scipy
+from .Particles import Particles
+
+class Environment:
+    """
+    Class environment sets how the particles interact with the environment.
+    """    
+    def __init__( self, room_size, acs_hole_area, acb_hole_area, heat_zone):
+        """
+        Define the properties of the environment.
+        room_size: size of the room (only support rectangle room) [xmin,xmax,ymin,ymax]
+        acs_hole_pos: position of the air conditioner sucking hole [xmin,xmax,ymin,ymax]
+        acb_hole_pos: position of the air conditioner blowing hole [xmin,xmax,ymin,ymax]
+        heat_zone: size of the area that increase temperature of particles [xmin,xmax,ymin,ymax]
+        """
+        self.room_size     = np.array(room_size)
+        self.acs_hole_area = np.array(acs_hole_area)
+        self.acb_hole_area = np.array(acb_hole_area)
+        self.acs_hole_dot  = np.array([(acs_hole_area[0]+acs_hole_area[1])/2,\
+                                      (acs_hole_area[2]+acs_hole_area[3])/2])
+        self.heat_zone     = np.array(heat_zone)
+        
+
+
+    def wall_bounce(self,particle):
+        """
+        This function will simulate how particles interact with the boundary(wall) .
+        particle: a Particles object
+        """
+        num = particle.nparticles
+        pos = particle.pos
+        vel = particle.vel
+        r_pos = self.room_size
+        # start the loop to check every particle in the particles
+        for n in range(num):
+            # check if the particle is out of the x boundary
+            if pos[n,0] < r_pos[0]:
+                vel[n,0] = -vel[n,0]
+                pos[n,0] = 2 * r_pos[0] - pos[n,0] 
+            elif pos[n,0] > r_pos[1]:
+                vel[n,0] = -vel[n,0]
+                pos[n,0] = 2 * r_pos[1] - pos[n,0]
+            # check if the particle is out of the y boundary
+            if pos[n,1] < r_pos[2]:
+                vel[n,1] = -vel[n,1]
+                pos[n,1] = 2 * r_pos[2] - pos[n,1]
+            elif pos[n,1] > r_pos[3]:
+                vel[n,1] = -vel[n,1]
+                pos[n,1] = 2 * r_pos[3] - pos[n,1]
+        return pos, vel
+
+    def air_con_suck(self,particle):
+        """
+        This function will simulate the air conditioner sucking air
+        """
+        dot     = self.acs_hole_dot
+        area    = self.acs_hole_area
+        n       = particle.nparticles
+        pos     = particle.pos
+        vel     = particle.vel
+        r       = pos - dot
+        dic_rot = np.cross(pos,r)
+        # start the loop to check every particle in the particles
+        #for i in range(n):
+        #    vel = 
+            
+
+
+
+    def air_con_blow(self):
+        """
+        This function will simulate the air conditioner blowing air
+        """
+        #TODO
+        pass
+
+    def heat_storage(self,T):
+        """
+        This function will simulate the hot spot in the room.
+        """
+        #TODO
+        pass
+
+
---
+++ functions/Particles.py
@@ -0,0 +1,112 @@
+import numpy as np
+import scipy.constants as const
+import scipy.stats as stats
+class Particles:
+    """
+    Class particles is the base on the particles we have on class. It contains the basic properties of particles.
+    It contains the following properties:
+
+    nparticles: number of particles 
+    time: time of the simulation
+    pos: position of the particles
+    vel: velocity of the particles
+    mass: mass of the particles
+    T:  average temperature of all particles
+    room_size: size of the room.(only support rectangle room)
+    pos_type: type of position distribution
+    vel_type: type of velocity distribution
+    molecular_weight: molecular weight of the particles
+    particles_radius: radius of the particles
+    """
+    def __init__(self,n):
+        """
+        Create empty lists for every property of the particles.
+        """
+        self.nparticles = n
+        self.time=0
+        self.pos=np.zeros((n,2))
+        self.vel=np.zeros((n,2))
+        return
+
+    def pos_distrib(self):
+        """
+        This function will distribute the particles randomly in certain distribution in the room.
+        Some parameters are needed to calculate the distribution:
+        n: number of particles
+        room_size: size of the room [xmin,xmax,ymin,ymax]
+        type: type of distribution (uniform, normal)
+        """
+        room_size=self.room_size
+        n=self.nparticles
+        type=self.pos_type
+        xmin,xmax,ymin,ymax=room_size
+        if type=='uniform':
+            self.pos[:,0]= np.random.uniform(xmin,xmax,(n)) 
+            self.pos[:,1]= np.random.uniform(ymin,ymax,(n))
+        elif type=='normal':
+            self.pos[:,0]= np.random.normal((xmin+xmax)/2,(xmax-xmin)/6,(n))
+            self.pos[:,1]= np.random.normal((ymin+ymax)/2,(ymax-ymin)/6,(n))
+        return
+
+    def vel_distrib(self):
+        """
+        This function will calculate the distribution velocity (temperature) of the room.
+        Some parameters are needed to calculate the distribution:
+        T: temperature of the room
+        m: mass of the particles
+        N: number of particles
+        vel_type: type of distribution (Boltzmann)
+        """
+        T=self.T
+        m=self.mass
+        N=self.nparticles
+        v_type=self.vel_type
+        k=const.Boltzmann
+        if v_type=='Boltzmann':
+            speed = stats.maxwell.rvs(scale=np.sqrt(k*T/ m), size=N) #scale=a in https://mathworld.wolfram.com/MaxwellDistribution.html
+            #detailed from https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.maxwell.html#ra6d46ce10274-1
+            theta = np.random.uniform(0, 2*np.pi, N) #random angle
+            self.vel[:,0] = speed*np.cos(theta)
+            self.vel[:,1] = speed*np.sin(theta)
+        else:
+            return ValueError('Distribution type not supported')
+        return
+    
+    def set_particles(self,pos_type='uniform',vel_type='Boltzmann',room_size=[0,50,0,50],T=300,molecular_weight=28.9,particles_radius='None'):
+        """
+        This function will set the properties of particles.
+        pos_type: type of position distribution (uniform, normal)
+        vel_type: type of velocity distribution (Boltzmann)
+        room_size: size of the room [xmin,xmax,ymin,ymax]
+        T: temperature of the particles(K) ex.room temperature=300
+        molecular_weight: molecular weight of the particles(amu) ex.air=28.9
+        particles_radius: radius of the particles(0.1nm=1e-10m) ex.air=1.55
+        """
+        self.mass=const.physical_constants['atomic mass constant'][0]*molecular_weight
+        self.particles_radius=particles_radius
+        self.T=T
+        self.room_size=room_size
+        self.pos_type=pos_type
+        self.vel_type=vel_type
+        self.molecular_weight=molecular_weight
+        self.vel_distrib()
+        self.pos_distrib()
+        return
+    
+    def count_average_T(self):
+        """
+        This function will calculate the average temperature of the particles.
+        """
+        return np.mean(np.linalg.norm(self.vel, axis=1)**2*self.mass/(3*const.Boltzmann))
+    
+    if __name__ == "__main__":
+        import numpy as np
+        from Particles import Particles
+        from DataProcesser import DataProcesser
+
+
+        particles_number=100000
+        particles=Particles(particles_number)
+        particles.set_particles(pos_type='uniform',vel_type='Boltzmann',room_size=[0,50,0,50],T=300,molecular_weight=28.9)
+        print(particles.count_average_T())
+        pass

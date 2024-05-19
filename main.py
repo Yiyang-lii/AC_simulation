12d51350c521b57e294a3c6074132425a4820f8e
quantankevin <kevin980034@gmail.com>
2024-05-20 00:05:20 +0800

    add set_num_threads

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import numpy as np
-
+import numba as nb
 from functions.Particles import Particles
 from functions.DataProcesser import DataProcesser
 from functions.Environment import Environment
 from functions.Simulators import Simulators
 
-
+nthreads = 8
+nb.set_num_threads(nthreads)
 particles_number=10000
 particles=Particles(particles_number)
 particles.set_particles(pos_type='uniform',vel_type='Boltzmann',room_size=[0,50,0,50],T=300,particle_type='air')

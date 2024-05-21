045cf5e09fef2904b7b53c7205def7d7179a347e
Yiyang-lii <q0410127@gmail.com>
2024-05-21 23:25:50 +0800

    add a few codes in simulators.py

--- functions/Simulators.py
+++ functions/Simulators.py
@@ -1,10 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+import numba as nb
+from Environment import Environment
+from Particles import Particles
+from DataProcesser import DataProcesser
+
 class Simulators:
     """
     Class simulator is base on the simulator we have on class. Which can simulate the evolution of particles.
     """
-    def __init__(self, n, m):
+    def __init__(self, particle: Particles):
         #TODO
-        pass
+        self.particle = Particles
+        self.time = 0;
+        # future work: try to read the last output file and resume if something unexpected interrupted the simulation
+        
 
     def evolve(self, dt:float, tmax:float):
         """
@@ -15,18 +26,29 @@ class Simulators:
         
         """
         #TODO
-        pass
 
     def next_step(self, dt:float):
         """
         This function will calculate the next step of the simulation.
         """
         #TODO
-        pass
-    
+        self.time += dt
+
     def next_step_collision(self, dt:float):
         """
         This function will calculate the next step of the simulation with collision.
         """
         #TODO
         pass
+
+if __name__ == "__main__":
+    
+    nthreads = 2
+    nb.set_num_threads(nthreads)
+    
+    particles_number = 10
+    particles = Particles(particles_number)
+    particles.set_particles(pos_type='uniform',vel_type='Boltzmann',room_size=[0,50,0,50],T=300,molecular_weight=28.9) 
+    
+    simulation = Simulators(particles)
+    simulation.evolve(dt=0.01, tmax=10)

025734e60d3dc847632bb9c717336d3233e44a83
quantankevin <kevin980034@gmail.com>
2024-05-29 22:45:28 +0800

    chore: Update particles.step and particles.dt in Simulators.py

--- main.py
+++ main.py
@@ -6,21 +6,24 @@ from functions.Environment import Environment
 from functions.Simulators import Simulators
 import gc
 import datetime
-print('start_time=',datetime.datetime.today())
-nthreads = 1
-
+import matplotlib.pyplot as plt
+start_time=datetime.datetime.today()
+print('start_time=',start_time)
+nthreads = 20
 nb.set_num_threads(nthreads)
 #'''
-#set range of environment
+#set environment
 envir = Environment(room_size=[0,5000,0,5000],heat_zone_size=[2400,2500,0,2500])
+filepaths='data/test'
+filename='AC_simulation'
 
+#particles=DataProcesser.data_input(f'{filepaths}/{filename}_t00164.bin')   
 
 #set particles
 particles_number=1000
 particles=Particles(particles_number)
-particles.set_particles(pos_type='uniform',vel_type='Boltzmann',room_size=envir.room_size,T=310,particles_radius=1,molecular_weight=28.9)
+particles.set_particles(pos_type='uniform',vel_type='Boltzmann',room_size=envir.room_size,T=310,particles_radius=10,molecular_weight=28.9)
 
-   
 
 #set simulation
 simulation = Simulators(particles, envir)
@@ -29,14 +32,11 @@ simulation = Simulators(particles, envir)
 #set time step and time intial and final
 dt = 0.01
 t_init = 0
-tmax = 1
+tmax = 10
 
 
 #particles.vel[:, 0] *= 0.01
 #particles.vel[:, 1] *= 0.01 
-
-filepaths = 'data/N100000_t0_0.1_100_Collision_off_HeatZone_on'
-filename  = 'AC_simulation'
 
 
 #filepaths=f'data/N{particles_number}_t{t_init}_{dt}_{tmax}_Collision_off_HeatZone_[2400,2500,0,2500]/'
@@ -46,21 +46,25 @@ filename  = 'AC_simulation'
 time_arr = np.linspace(t_init, tmax, int((tmax - t_init) / dt) + 1)
 
 for i in range(len(time_arr)):
-    particles.dt=dt
-    particles.step = i
-    simulation.evolve(dt=dt, collision=False)
-    particles.pos,particles.vel = envir.ac_suck_and_blow(particles,T=290)
-    particles.vel = envir.heat_zone_add_temperature(particles,T=310)
-
+    
+    particles.pos,particles.vel,particles.step,particles.dt=simulation.evolve(dt=dt, collision=True)
+    #print('particles.vel=',particles.vel)
+    particles.pos,particles.vel=envir.ac_suck_and_blow(particles,T=290)
+    #particles.vel=envir.heat_zone_add_temperature(particles,T=310)
+    particles.vel = envir.ac_suck_behavior(particles)
     if i % 1 == 0:  
-        print("\r simulated time: {:.2f} out of {} , len(v) = {}".format(particles.step*particles.dt, tmax, len(particles.vel)) , end='')
+        print("time: ", "{:.2f}".format(particles.step*particles.dt))
         #count average temperature of the particles in each step
-        particles.T=Particles.count_average_T(particles)
+        #particles.T=Particles.count_average_T(particles)
         #save data in each steps
-  
+       # print('particles.vel=',particles.vel)
         DataProcesser.data_output(particles, filepaths, filename)
+        time=datetime.datetime.today()-start_time
+        time=time/(i+1)*(len(time_arr)-(i+1))
+        print(f'{round(i/len(time_arr)*100,2)}% is done,  estimated remaining time={time}')
+#==========================Save movie=================================  
 #generate the velocity distribution plot of last step to check the velocity distribution
-DataProcesser.plot_velocity_distribution(particles.T, particles.mass, particles.vel)
+#DataProcesser.plot_velocity_distribution(particles.T, particles.mass, particles.vel)
 
 
 #release memory
@@ -68,15 +72,19 @@ gc.collect()
 #'''
 #==========================Save movie=================================
 #load data
+'''
 
-filepaths = 'data/N100000_t0_0.1_100_Collision_off_HeatZone_on'
-filename  = 'AC_simulation'
+filepaths='data/collision_suck_blow_n10000'
+filename='collision_suck_blow'
 fns=DataProcesser.load_files(filepaths,filename) 
-# print('fns=',fns)
-#particles=DataProcesser.data_input(f'{filepaths}{filename}_t00001.bin')   
+#print('fns=',fns)
+#particles=DataProcesser.data_input(f'{filepaths}/{filename}_t00001.bin')   
 #DataProcesser.plot_velocity_distribution(particles.T, particles.mass, particles.vel)  
 #output movie  
 #DataProcesser.output_movie(fns, resolution=200, sigma=10, filename="temperature.mp4", fps=10, plot_func="plot_gas_temperature") #plot temperature
-DataProcesser.output_movie(fns, resolution=200, sigma=0, filename="number_density_test.mp4", fps=10, plot_func="plot_gas_number_density") #plot number density
+DataProcesser.output_particles_movie(fns, envir.room_size, filename='collision_suck_blow_n10000.mp4', fps=30) #plot particles
+#DataProcesser.output_movie(fns, resolution=200, sigma=0, filename="number_density_test.mp4", fps=10, plot_func="plot_gas_number_density") #plot number density
+'''
+end_time=datetime.datetime.today()
 print('end_time=',datetime.datetime.today())
-print('time_cost=',datetime.datetime.today()-datetime.datetime.today())
+print('time_cost=',end_time-start_time)
--- test.py
+++ test.py
@@ -7,19 +7,22 @@ from functions.Simulators import Simulators
 import gc
 import datetime
 import matplotlib.pyplot as plt
-print('start_time=',datetime.datetime.today())
+start_time=datetime.datetime.today()
+print('start_time=',start_time)
 nthreads = 20
 nb.set_num_threads(nthreads)
 #'''
 #set environment
 envir = Environment(room_size=[0,5000,0,5000],heat_zone_size=[2400,2500,0,2500])
-
-
+filepaths='data/test'
+filename='AC_simulation'
+particles=DataProcesser.data_input(f'{filepaths}/{filename}_t000124.bin')   
+'''
 #set particles
-particles_number=1000
+particles_number=10000
 particles=Particles(particles_number)
 particles.set_particles(pos_type='uniform',vel_type='Boltzmann',room_size=envir.room_size,T=310,particles_radius=10,molecular_weight=28.9)
-
+'''
 #set simulation
 simulation = Simulators(particles, envir)
 
@@ -32,8 +35,7 @@ tmax = 10
 
 #particles.vel[:, 0] *= 0.01
 #particles.vel[:, 1] *= 0.01 
-filepaths='data/test'
-filename='AC_simulation'
+
 
 #filepaths=f'data/N{particles_number}_t{t_init}_{dt}_{tmax}_Collision_off_HeatZone_[2400,2500,0,2500]/'
 #filename='AC_simulation'
@@ -67,15 +69,16 @@ gc.collect()
 #==========================Save movie=================================
 #load data
 
-filepaths='data/test'
-filename='AC_simulation'
+filepaths='data/collision_suck_blow_n10000'
+filename='collision_suck_blow'
 fns=DataProcesser.load_files(filepaths,filename) 
 #print('fns=',fns)
 #particles=DataProcesser.data_input(f'{filepaths}/{filename}_t00001.bin')   
 #DataProcesser.plot_velocity_distribution(particles.T, particles.mass, particles.vel)  
 #output movie  
 #DataProcesser.output_movie(fns, resolution=200, sigma=10, filename="temperature.mp4", fps=10, plot_func="plot_gas_temperature") #plot temperature
-DataProcesser.output_particles_movie(fns, envir.room_size, filename='collision_suck_blow.mp4', fps=30) #plot particles
+DataProcesser.output_particles_movie(fns, envir.room_size, filename='collision_suck_blow_n10000.mp4', fps=30) #plot particles
 #DataProcesser.output_movie(fns, resolution=200, sigma=0, filename="number_density_test.mp4", fps=10, plot_func="plot_gas_number_density") #plot number density
+end_time=datetime.datetime.today()
 print('end_time=',datetime.datetime.today())
-print('time_cost=',datetime.datetime.today()-datetime.datetime.today())
+print('time_cost=',start_time-end_time)
